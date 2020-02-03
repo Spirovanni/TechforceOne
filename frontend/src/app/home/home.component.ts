107766643361f60f8048a4969b1daa1cfc3e23f3
Xavier Martinez <blackshieldsx@gmail.com>
2020-02-02 19:50:12 -0800

    Fix home.componet to connect to proper settings

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { takeWhile } from 'rxjs/operators';
+import { NbTokenService } from '@nebular/auth';
+import { NbMenuItem } from '@nebular/theme';
+import { HomeMenu } from './home-menu';
+import { InitUserService } from '../@theme/services/init-user.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy {
 
-  constructor() { }
+  menu: NbMenuItem[];
+  alive = true;
 
-  ngOnInit() {
+  constructor(
+    private homeMenu: HomeMenu,
+    private tokenService: NbTokenService,
+    protected initUserService: InitUserService,
+  ) {
+    this.initMenu();
+
+    this.tokenService.tokenChange()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(() => {
+        this.initMenu();
+      });
   }
 
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  initMenu() {
+    this.homeMenu.getMenu()
+      .pipe(takeWhile(() => this.alive))
+      .subscribe(menu => {
+        this.menu = menu;
+      });
+  }
+  ngOnDestroy(): void {
+    this.alive = false;
+  }
 }

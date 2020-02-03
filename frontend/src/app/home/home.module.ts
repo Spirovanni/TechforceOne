b0f9ad2f3d715ffae36d22c117d2ee2b83f755fc
Xavier Martinez <blackshieldsx@gmail.com>
2020-02-02 19:14:55 -0800

    Create Home Menu, Routing and Module

--- frontend/src/app/home/home.module.ts
+++ frontend/src/app/home/home.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
+import {} from './home-routing.module'
 
-const Home_COMPONENTS = [
+const HOME_COMPONENTS = [
   HomeComponent,
 ];
 
+@NgModule({
+  imports: [
+    // PagesRoutingModule,
+    // ThemeModule,
+    // DashboardModule,
+    // ECommerceModule,
+    // NbMenuModule,
+    // MiscellaneousModule,
+    // AuthModule.forRoot(),
+  ],
+  declarations: [
+    ...HOME_COMPONENTS,
+  ],
+  providers: [
+    PagesMenu,
+  ],
+})
+export class HomeModule {
+}
